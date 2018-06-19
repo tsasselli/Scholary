8aa46ff2228e749735de7b75b317ec47bcdb0ed6
tsasselli <tsasselli@gmail.com>
2018-06-18 19:16:04 -0700

    added routing for school components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,15 @@
-import { AuthService } from './core/auth.service';
-import { CoreRoutingModule, coreRoutedComponents } from './core/core-routing.module';
-import { CoreModule } from './core/core.module';
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AuthService } from './core/auth.service';
+import { CoreModule } from './core/core.module';
 import { SchoolComponent } from './schools/components/school/school.component';
-import { NewSchoolComponent } from './schools/components/new-school/new-school.component';
-import { NewClassComponent } from './classes/components/new-class/new-class.component';
-import { UserProfileComponent } from './core/components/user-profile/user-profile.component';
-import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SchoolComponent,
-    NewSchoolComponent,
-    NewClassComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/core/core-routing.module.ts
+++ src/app/core/core-routing.module.ts
@@ -1,12 +1,17 @@
+import { SchoolComponent } from './../schools/components/school/school.component';
+import { NewClassComponent } from './../classes/components/new-class/new-class.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { UserProfileComponent } from './components/user-profile/user-profile.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WelcomeComponent } from './components/welcome/welcome.component';
+import { NewSchoolComponent } from '../schools/components/new-school/new-school.component';
 
 const routes: Routes = [
   { path: '', component: WelcomeComponent },
   { path: 'login', component: UserProfileComponent },
+  { path: 'school', component: SchoolComponent},
+  { path: 'school/new', component: NewSchoolComponent },
   { path: '**', component: NotFoundComponent}
 ];
 
@@ -16,4 +21,4 @@ const routes: Routes = [
 })
 export class CoreRoutingModule { }
 
-export const coreRoutedComponents = [UserProfileComponent, WelcomeComponent ];
+export const coreRoutedComponents = [UserProfileComponent, WelcomeComponent, SchoolComponent, NewSchoolComponent ];
