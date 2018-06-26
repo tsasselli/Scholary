766d0845547763cc653ab54960b50cc5c1c805c8
tsasselli <tsasselli@gmail.com>
2018-06-26 12:41:10 -0700

    fetching class data

--- src/app/classes/class/class.component.ts
+++ src/app/classes/class/class.component.ts
@@ -30,6 +30,7 @@ export class ClassComponent implements OnInit {
             
       //     })
        })
+       
     )
   }
 
--- src/app/services/class.service.ts
+++ src/app/services/class.service.ts
@@ -10,14 +10,34 @@ import { map } from 'rxjs/operators';
 export class ClassService {
 
   class$: Observable<Class[]>;
+  classRef: AngularFirestoreCollection<Class>;
+  class: any;
 
-  constructor(public afs: AngularFirestore) { 
+  constructor(public afs: AngularFirestore) {
+    this.classRef = this.afs.collection('class');
+    this.class$ = this.classRef.valueChanges(); 
   }
 
   getClass(school) {
-     return this.afs.collection('school').doc('name').collection('class', ref => {
-     return ref.where('schoolId', '==', school.name)
-    });
+    return this.afs.collection('class').valueChanges().pipe(
+      map(data => {
+        console.log(data);
+        return data
+        })
+    )
+    // return this.afs.collection('class').doc(`${school}`).snapshotChanges().pipe(
+    //   map(a => {
+    //       const data = a.payload.data() as Class;
+    //       const id = a.payload.id;
+    //       console.log(id);
+    //       console.log(data);
+    //       return { id, data };
+    //     })
+    // )
+
+    //  return this.afs.collection('class', ref => {
+    //  return ref.where('schoolId', '==', school)
+    //});
         // const classCollection: AngularFirestoreCollection<Class> = this.afs.collection('school').doc(`${schoolName}`).collection('classes');
     // return classCollection.snapshotChanges().pipe(
     //   map(changes => {
